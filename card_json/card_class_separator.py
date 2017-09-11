1932de13890062298599aa1e7e30e7b1d44605c0
Jeremy Deutsch <jeremydeutsch@email.arizona.edu>
2017-09-10 23:15:58 -0700

    Improved the scrubber readability, and added a script to split the master card file into multiple class files.

---
+++ card_json/card_class_separator.py
@@ -0,0 +1,38 @@
+import json
+
+## A script that splits a JSON master file for a HearthCube into separate files for each HS class (Druid, Priest, etc.)
+
+# First, load the entirety of the cube's master file into memory.
+master_cards_file = open('cards.cube.master.json', 'r')
+all_cards = json.load(master_cards_file)
+master_cards_file.close()
+
+# We create the file for each class separately, instead of writing to them continuously, 
+# or having all the classes' data coexist as one giant dictionary.
+for cardClass in ['PRIEST', 'MAGE', 'WARLOCK', 'WARRIOR', 'HUNTER', 'PALADIN', 'ROGUE', 'DRUID', 'SHAMAN']:
+    cube_class = {
+        'COMMON': [],
+        'RARE': [],
+        'EPIC': [],
+        'LEGENDARY': []
+    }
+    # We need to parse each rarity, since they're separated in this format.
+    for rarity in ['COMMON', 'RARE', 'EPIC', 'LEGENDARY']:
+        for card in all_cards[rarity]:
+            if card['cardClass'] == 'NEUTRAL' or card['cardClass'] == cardClass:
+                new_card = {}
+                new_card['id'] = card['id']
+                new_card['dbfId'] = card['dbfId']
+                new_card['name'] = card['name']
+                new_card['rarity'] = card['rarity']
+                new_card['cardClass'] = card['cardClass']
+                # In the class files, cards only have one frequency.
+                if cardClass in card['frequency']:
+                    new_card['frequency'] = card['frequency'][cardClass]
+                else:
+                    new_card['frequency'] = card['frequency']['DEFAULT']
+                cube_class[new_card['rarity']].append(new_card)
+    # Write each class' cube to another file
+    cube_class_file = open('cards.cube.' + cardClass.lower() + '.json', 'w')
+    json.dump(cube_class, cube_class_file)
+    cube_class_file.close()
--- card_json/card_scrubber.py
+++ card_json/card_scrubber.py
@@ -1,14 +1,22 @@
 import json
 
+## A script that takes the JSON of every collectible Hearthstone card, and turns it into a useful format
+## for a HearthCube master JSON file.
+
+# First, load the HS card master set into memory.
 all_cards_file = open('cards.collectible.json', 'r')
 all_cards = json.load(all_cards_file)
 all_cards_file.close()
+
+# The master file will separate cards by rarity.
 cube_master = {
     'COMMON': [],
     'RARE': [],
     'EPIC': [],
     'LEGENDARY': []
 }
+
+# Parse all the cards, and add the fields we care about to our new set.
 for card in all_cards:
     new_card = {}
     new_card['id'] = card['id']
@@ -26,6 +34,7 @@ for card in all_cards:
         new_card['frequency'][new_card['cardClass']] = 1.0
     cube_master[new_card['rarity']].append(new_card)
 
+# Create a new JSON file, with indents for easy reading
 cube_master_file = open('cards.cube.all.json', 'w')
-json.dump(cube_master, cube_master_file)
+json.dump(cube_master, cube_master_file, indent=1)
 cube_master_file.close()
